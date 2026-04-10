43aecf6182b5f000ff938562dee7f575fdd124fb
Eslam Salah <eslam.salah74@gmail.com>
2026-04-10 14:40:19 +0200

    Installing Laravel + Adding Docker Settings + Building Database + Building File Structure

---
+++ app/resources/Product/ProductResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Resources\Product;
+
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            
+        ];
+    }
+}
